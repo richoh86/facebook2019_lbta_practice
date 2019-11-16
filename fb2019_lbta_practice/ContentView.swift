dfd969abe6ae03538497a1f09716015be0898a6a
richoh86 <wsoh1986@gmail.com>
2019-11-16 23:57:00 +0900

    Initial Commit

---
+++ fb2019_lbta_practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fb2019_lbta_practice
+//
+//  Created by richoh86 on 2019/11/16.
+//  Copyright © 2019 richoh86. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
